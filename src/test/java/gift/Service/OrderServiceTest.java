59932c5f2d688952785a6f2f9a1e99abf4764236
jyhyt1567 <jyhyt1567@gmail.com>
2025-07-30 17:09:36 +0900

    test: 테스트 코드 수정

--- src/test/java/gift/Service/OrderServiceTest.java
+++ src/test/java/gift/Service/OrderServiceTest.java
@@ -52,15 +52,13 @@ public class OrderServiceTest {
     void 구매_성공_테스트() {
         CreateOrderRequestDto requestDto = new CreateOrderRequestDto(option.getId(), 2L, message);
         Order order = new Order(1L, option, requestDto.quantity(),message);
-        given(optionService.findOptionByIdOrElseThrow(option.getId())).willReturn(option);
         given(optionService.purchaseOption(option.getId(), requestDto.quantity()))
-                .willReturn(new OptionResponseDto(
+                .willReturn(new Option(
                         option.getId(),
                         option.getName(),
-                        option.getQuantity() - requestDto.quantity()));
+                        option.getQuantity() - requestDto.quantity(),
+                        option.getProduct()));
         given(orderRepository.save(any())).willReturn(order);
-        given(wishService.findMemberWishByProductId(product.getId(), memberId))
-                .willReturn(Optional.empty());
 
         OrderResponseDto actual = orderService.purchaseProduct(requestDto, memberId);
 
@@ -76,7 +74,6 @@ public class OrderServiceTest {
     @DisplayName("옵션 수량 부족 시 구매 실패 테스트")
     void 수량_부족_구매_실패_테스트() {
         CreateOrderRequestDto requestDto = new CreateOrderRequestDto(option.getId(), 9999L, message);
-        given(optionService.findOptionByIdOrElseThrow(option.getId())).willReturn(option);
         given(optionService.purchaseOption(option.getId(), requestDto.quantity()))
                 .willThrow(new CustomException(ErrorCode.OptionNotEnough));
 
@@ -89,7 +86,7 @@ public class OrderServiceTest {
     @DisplayName("없는 옵션 구매 시 구매 실패 테스트")
     void 없는_옵션_구매_실패_테스트() {
         CreateOrderRequestDto requestDto = new CreateOrderRequestDto(999L, 2L, message);
-        given(optionService.findOptionByIdOrElseThrow(999L))
+        given(optionService.purchaseOption(999L, 2L))
                 .willThrow(new CustomException(ErrorCode.OptionNotFound));
         CustomException e = Assertions.assertThrows(CustomException.class,
                 () -> orderService.purchaseProduct(requestDto, memberId));
